d996194c040aa0987e2ea2800181080a154a0a1a
dylan-marx <dylanmarx0404@gmail.com>
2024-08-01 16:50:54 +0200

    text changes green if it's correct

--- src/components/TextStage.jsx
+++ src/components/TextStage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 const TextStage = () => {
-    const [input, setInput] = useState("");
+    const [typed, setTyped] = useState("");
     const [outstanding, setOutstanding] = useState("Hi, this is an example of text that you can practice with. Please feel free to provide your own should you so desire.");
     useEffect(() => {
         document.getElementById("text-stage").focus();
@@ -20,7 +20,9 @@ const TextStage = () => {
 
         // If the keys match up change the color
         if (e.key == outstanding[0]) {
+            
             /* Add it in a span that changes the colors*/
+            setTyped(typed + e.key);
             setOutstanding(outstanding.slice(1));
         }
 
@@ -35,8 +37,10 @@ const TextStage = () => {
 
     return (
         <div>
-            <div id="text-stage" className="caret-transparent text-4xl text-blue-200 focus:outline-none p-4 lg:px" tabIndex="0" contentEditable onKeyDown={handleTyping}>
+            <div id="text-stage" className="caret-transparent text-4xl text-blue-200 focus:outline-none p-4 lg:px-20" tabIndex="0" contentEditable onKeyDown={handleTyping}>
+                <span className="text-green-400">{typed}</span>
                 {outstanding}
+                
             </div>
  
             <div id="text-done" className="text-4xl text-blue-200 hidden">
