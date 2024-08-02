dc4e91ca18a43ce6c6f0352a172c9a6d4d33c94d
dylan-marx <dylanmarx0404@gmail.com>
2024-08-02 14:07:52 +0200

    fixed spaces dissapearing as well as bug where spaces would cause you to win when typed as last char

--- src/components/TextStage.jsx
+++ src/components/TextStage.jsx
@@ -4,7 +4,8 @@ const TextStage = () => {
     const [typed, setTyped] = useState("");
     const [wrong, setWrong] = useState("");
     const [replace, setReplace] = useState("");
-    const [outstanding, setOutstanding] = useState("Hi, this is an example of text that you can practice with. Please feel free to provide your own should you so desire.");
+    const [outstanding, setOutstanding] = useState("example");
+    const [initialText, setInitialText] = useState(outstanding);
     useEffect(() => {
         document.getElementById("text-stage").focus();
     }, []);
@@ -23,43 +24,61 @@ const TextStage = () => {
 
         document.getElementById("begin-button").classList.add("hidden");
 
-        if (e.key === 'Backspace' && replace) {
+        // If backspace is pressed replace the character
+        if (e.key === 'Backspace' && replace.length >= 1 && replace.length == wrong.length) {
             setWrong(wrong.slice(0, wrong.length - 1));
             setOutstanding(replace[0] + outstanding);
             setReplace(replace.slice(1));
+        } else if (wrong.length > 0) {
+            setWrong(wrong.slice(0, wrong.length - 1));
         }
 
-        console.log(e.code);
+        // If the key pressed is an accepted value process it
         if ((e.which >= 48 && e.which <= 90) || (e.which >=186 && e.which <= 222) || e.which === 32) {
-            if (e.key == outstanding[0] && wrong.length === 0) {
+            if (e.key === outstanding[0] && wrong.length === 0) {
                 setTyped(typed + e.key);
                 setOutstanding(outstanding.slice(1));
-            } else if (e.key !== "Backspace" && e.key !== "Shift") {
+            } else {
                 setWrong(wrong + e.key);
-                setReplace(outstanding[0] + replace);
+                if (outstanding.length > 0) {
+                    setReplace(outstanding[0] + replace);
+                }
                 setOutstanding(outstanding.slice(1));
+                }
             }
         }
-    }
+
+        console.log("outstanding => " + outstanding);
+        console.log("wrong => " + wrong);
+        console.log("replace => " + replace);
+    
 
     const begin = () => {
         document.getElementById("text-stage").focus();
         document.getElementById("begin-button").classList.add("hidden");
     }
 
+    const reset = () => {
+        location.reload();
+        setOutstanding(initialText);
+        document.getElementById("text-stage").focus();
+        document.getElementById("begin-button").classList.add("hidden");
+        document.getElementById("text-done").classList.add("hidden");
+    }
+
     return (
         <div className="flex flex-col justify-center items-center" spellCheck="false">
-            <div id="text-stage" className="caret-transparent text-4xl text-blue-200 focus:outline-none p-4 lg:px-20 py-10 no-underline" tabIndex="0" contentEditable onKeyDown={handleTyping} suppressContentEditableWarning={true}>
+            <div id="text-stage" className="caret-transparent text-4xl text-blue-200 focus:outline-none p-4 lg:cpx-20 py-10 no-underline whitespace-pre-wrap" tabIndex="0" contentEditable onKeyDown={handleTyping} suppressContentEditableWarning={true}>
                 <span className="text-green-400">{typed}</span>
-                <span className="text-red-400 underline">{wrong}</span>
+                <span className="bg-red-400">{wrong}</span>
                 {<span className="underline underline-offset-2">{outstanding[0]}</span>}
                 {outstanding.slice(1)}
                 
             </div>
  
-            <div id="text-done" className="text-4xl text-blue-200 hidden">
+            <div id="text-done" className=" flex flex-col justify-center items-center gap-4 text-4xl text-blue-200 hidden">
                 <p>WOW you did it!!!</p>
-                <button onClick={() => location.reload()} className="text-4xl text-blue-200 border-4 p-2 border-blue-200">Reset</button>
+                <button onClick={reset} className="text-4xl text-blue-200 border-4 p-2 border-blue-200">Reset</button>
             </div>
 
             <button id="begin-button" onClick={begin} className="text-4xl text-blue-200 border-4 p-2 border-blue-200">Begin</button>
