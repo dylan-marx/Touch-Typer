51ba5a0c9b264473e2bd8f6c4794178493e1c0c0
dylan-marx <dylanmarx0404@gmail.com>
2024-08-02 08:45:38 +0200

    started on show text as red when typed incorrectly

--- src/components/TextStage.jsx
+++ src/components/TextStage.jsx
@@ -2,43 +2,51 @@ import { useEffect, useState } from "react";
 
 const TextStage = () => {
     const [typed, setTyped] = useState("");
+    const [wrong, setWrong] = useState("");
+    const [replace, setReplace] = useState("");
     const [outstanding, setOutstanding] = useState("Hi, this is an example of text that you can practice with. Please feel free to provide your own should you so desire.");
     useEffect(() => {
         document.getElementById("text-stage").focus();
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        if (outstanding.length === 0) {
+            document.getElementById("text-stage").classList.add("hidden");
+            document.getElementById("text-done").classList.remove("hidden");
+        }
+    }, [outstanding])
     /**
      * Handles main logic in regards to typing
      */
     const handleTyping = (e) => {
         e.preventDefault();
-        console.log(e.key)
-        if (e.key === 'Backspace') {
+
+        if (e.key === 'Backspace' && replace) {
+            setWrong(wrong.slice(0, wrong.length - 1));
+            setOutstanding(replace[0] + outstanding);
+            setReplace(replace.slice(1));
             /* TODO handle backspace */
         } else if (e.key === "  ") {
             /* TODO handle space */
         }
 
         // If the keys match up change the color
-        if (e.key == outstanding[0]) {
-            
-            /* Add it in a span that changes the colors*/
+        if (e.key == outstanding[0] && wrong.length === 0) {
             setTyped(typed + e.key);
+            setOutstanding(outstanding.slice(1));
+        } else if (e.key !== "Backspace" && e.key !== "Shift") {
+            setWrong(wrong + e.key);
+            setReplace(outstanding[0] + replace);
             setOutstanding(outstanding.slice(1));
         }
 
-        // If the outstanding text is exhausted display victory screen
-        if (outstanding.length === 1 || !outstanding) {
-            document.getElementById("text-stage").classList.add("hidden");
-            document.getElementById("text-done").classList.remove("hidden");
-        }
-
-        console.log(outstanding.length);
     }
 
     return (
         <div>
-            <div id="text-stage" className="caret-transparent text-4xl text-blue-200 focus:outline-none p-4 lg:px-20" tabIndex="0" contentEditable onKeyDown={handleTyping}>
+            <div id="text-stage" className="caret-transparent text-4xl text-blue-200 focus:outline-none p-4 lg:px-20" tabIndex="0" contentEditable onKeyDown={handleTyping} suppressContentEditableWarning={true}>
                 <span className="text-green-400">{typed}</span>
+                <span className="text-red-400">{wrong}</span>
                 {outstanding}
                 
             </div>
